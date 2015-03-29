91a0f6f6b921bfbefd35c77cc1c4ecbf0def2fb7
Sunia <skalra17@yahoo.in>
2015-03-29 22:46:58 +0530

    Link changes

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -12,7 +12,7 @@ class EmailMailer < ApplicationMailer
         user = User.find(sender)
         @random_key = SecureRandom.hex
 
-        @shorten_link = "https://sheltered-crag-1698.herokuapp.com/reply/" + @random_key
+        @shorten_link = "https://ques-era.herokuapp.com/reply/" + @random_key
         #@shorten_link = "localhost:3000/reply/" + @random_key
         Sender.create(:email_id => email.id, :user_id => user.id, :unique_key => @random_key)
         mail(to: user.email, subject: 'Please answer the Question') #if @listener != user.username
